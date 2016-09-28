7a46b9ad9e467e27ec3e78af2c59c98e4d333e02
Dorichev Egor <egordorichev@gmail.com>
2016-09-28 20:07:39 +0300

    Откат

--- jalrti.py
+++ jalrti.py
@@ -1,38 +1,5 @@
-#importing
-import re
 
-#debug mode
 debug = True
-if debug == True:
+
+if debug:
 	print("Debug mode is on")
-else:
-	print("Debug mode is off")
-
-#global initialization
-global_varibles = {}
-functions = {}
-
-#functions definding
-def main():
-
-
-def functions_parser(code):
-	#initialization
-
-	#code
-	if not "func " in code :
-		return -1
-	start_index = code.find("func ")
-	if bracket_index = code.find("(") == -1:
-		return -1
-	name = code[start_index + 5 : bracket_index - 1]
-
-def varibles_parser(code):
-	#initialization
-	if not "var " in code:
-		return -1 
-
-
-#run the programm
-if __name__ == "__main__":
-    main()
