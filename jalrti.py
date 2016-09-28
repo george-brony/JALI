aeb1aa0ad4e0bb8c60c4a4bbf86e8c775a2ea48d
george-brony <george.shadarck@egmail.com>
2016-09-28 19:45:03 +0300

    start developing function parser

--- jalrti.py
+++ jalrti.py
@@ -1,6 +1,37 @@
-debug = True
+#importing
+import re
 
+#debug mode
+debug = True
 if debug == True:
 	print("Debug mode is on")
 else:
 	print("Debug mode is off")
+
+#global initialization
+global_varibles = {}
+functions = {}
+
+#functions definding
+def main():
+
+
+def functions_parser(code):
+	#initialization
+
+	#code
+	if not "func " in code :
+		return None
+	start_index = code.find("func ")
+	if bracket_index = code.find("(") == -1:
+		return None
+	name = code[start_index : bracket_index]
+
+def varibles_parser(code):
+	#initialization
+
+
+
+#run the programm
+if __name__ == "__main__":
+    main()
